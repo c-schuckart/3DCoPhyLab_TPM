Fixed mistakes in the diffusion equation. Testing of dynamic diffusion shows positive results. Problem with outgassing rate while using sintering, specifically the area calculation is noted.

diffusion_equation_DG_ADI.py
@@ -112,9 +112,9 @@ def boundary_condition_implicit_z_sweep_de_zfirst(dt, gas_mass, Diffusion_coeffi
 
 
 @njit
-def set_matrices_lhs_y_sweep_de(n_y, i, k, sub_alpha, diag, sub_gamma, Diffusion_coefficient, dx, dy, dz, Dr, dt, S_p, gas_mass, surface, sh_adjacent_voxels, temperature):
+def set_matrices_lhs_y_sweep_de(n_y, i, k, sub_alpha, diag, sub_gamma, Diffusion_coefficient, dx, dy, dz, Dr, dt, S_p, gas_mass, surface, sh_adjacent_voxels, temperature, simulate_region):
     for j in range(0, n_y):
-        if temperature[i][j][k] > 0 and np.sum(surface[i][j][k]) == 0 and diag[j] == 0:   #The diag == 0 condition is needed for cases where there is an 'interior' top boundary condition
+        if (temperature[i][j][k] > 0 or simulate_region[i][j][k] == 1) and np.sum(surface[i][j][k]) == 0 and diag[j] == 0:   #The diag == 0 condition is needed for cases where there is an 'interior' top boundary condition
             a_n = 1 / 2 * Diffusion_coefficient[i][j][k][2] * dx[i][j][k] * dz[i][j][k]/Dr[i][j][k][2] * (1 - sh_adjacent_voxels[i][j][k][2])
             a_s = 1 / 2 * Diffusion_coefficient[i][j][k][3] * dx[i][j][k] * dz[i][j][k]/Dr[i][j][k][3] * (1 - sh_adjacent_voxels[i][j][k][3])
             sub_alpha[j] = - a_n
@@ -220,9 +220,9 @@ def boundary_condition_implicit_y_sweep_de_zfirst(dt, gas_mass, z_sweep_gas_mass
 
 
 @njit
-def set_matrices_lhs_x_sweep_de(n_x, i, j, sub_alpha, diag, sub_gamma, Diffusion_coefficient, dx, dy, dz, Dr, dt, S_p, gas_mass, surface, sh_adjacent_voxels, temperature):
+def set_matrices_lhs_x_sweep_de(n_x, i, j, sub_alpha, diag, sub_gamma, Diffusion_coefficient, dx, dy, dz, Dr, dt, S_p, gas_mass, surface, sh_adjacent_voxels, temperature, simulate_region):
     for k in range(0, n_x):
-        if temperature[i][j][k] > 0 and np.sum(surface[i][j][k]) == 0 and diag[k] == 0:   #The diag == 0 condition is needed for cases where there is an 'interior' top boundary condition
+        if (temperature[i][j][k] > 0 or simulate_region[i][j][k] == 1) and np.sum(surface[i][j][k]) == 0 and diag[k] == 0:   #The diag == 0 condition is needed for cases where there is an 'interior' top boundary condition
             a_e = 1/2 * Diffusion_coefficient[i][j][k][4] * dy[i][j][k] * dz[i][j][k]/Dr[i][j][k][4] * (1 - sh_adjacent_voxels[i][j][k][4])
             a_w = 1/2 * Diffusion_coefficient[i][j][k][5] * dy[i][j][k] * dz[i][j][k]/Dr[i][j][k][5] * (1 - sh_adjacent_voxels[i][j][k][5])
             sub_alpha[k] = - a_e
@@ -503,9 +503,9 @@ def de_implicit_DGADI_zfirst(n_x, n_y, n_z, surface_reduced, dt, gas_mass, Diffu
 
 
 @njit
-def set_matrices_lhs_z_sweep_de_periodic(n_z, j, k, sub_alpha, diag, sub_gamma, Diffusion_coefficient, dx, dy, dz, Dr, dt, S_p, gas_mass, surface, sh_adjacent_voxels, temperature):
+def set_matrices_lhs_z_sweep_de_periodic(n_z, j, k, sub_alpha, diag, sub_gamma, Diffusion_coefficient, dx, dy, dz, Dr, dt, S_p, gas_mass, surface, sh_adjacent_voxels, temperature, simulate_region):
     for i in range(0, n_z):
-        if temperature[i][j][k] > 0 and np.sum(surface[i][j][k]) == 0 and diag[i] == 0:   #The diag == 0 condition is needed for cases where there is an 'interior' top boundary condition
+        if (temperature[i][j][k] > 0 or simulate_region[i][j][k] == 1) and np.sum(surface[i][j][k]) == 0 and diag[i] == 0:   #The diag == 0 condition is needed for cases where there is an 'interior' top boundary condition
             a_t = 1 / 2 * Diffusion_coefficient[i][j][k][0] * dx[i][j][k] * dy[i][j][k]/Dr[i][j][k][0] * (1 - sh_adjacent_voxels[i][j][k][0])
             a_b = 1 / 2 * Diffusion_coefficient[i][j][k][1] * dx[i][j][k] * dy[i][j][k]/Dr[i][j][k][1] * (1 - sh_adjacent_voxels[i][j][k][1])
             sub_alpha[i] = - a_t
@@ -521,7 +521,7 @@ def set_matrices_lhs_z_sweep_de_periodic(n_z, j, k, sub_alpha, diag, sub_gamma, 
 
 
 @njit
-def set_matrices_rhs_z_sweep_de_periodic(n_z, j, k, rhs, gas_mass, surface, dx, dy, dz, Dr, Diffusion_coefficient, dt, S_c, sh_adjacent_voxels, n_x, n_y, temperature):
+def set_matrices_rhs_z_sweep_de_periodic(n_z, j, k, rhs, gas_mass, surface, dx, dy, dz, Dr, Diffusion_coefficient, dt, S_c, sh_adjacent_voxels, n_x, n_y, temperature, simulate_region):
     x_neg_periodic, x_pos_periodic, y_neg_periodic, y_pos_periodic = 0, 0, 0, 0
     if k == 1:
         x_neg_periodic = 1
@@ -532,7 +532,7 @@ def set_matrices_rhs_z_sweep_de_periodic(n_z, j, k, rhs, gas_mass, surface, dx, 
     if j == n_y - 2:
         y_pos_periodic = 1
     for i in range(0, n_z):
-        if temperature[i][j][k] > 0 and np.sum(surface[i][j][k]) == 0 and rhs[i] == 0:
+        if (temperature[i][j][k] > 0 or simulate_region[i][j][k] == 1) and np.sum(surface[i][j][k]) == 0 and rhs[i] == 0:
             a_t = 1 / 2 * Diffusion_coefficient[i][j][k][0] * dx[i][j][k] * dy[i][j][k] / Dr[i][j][k][0] * (1 - sh_adjacent_voxels[i][j][k][0])
             a_b = 1 / 2 * Diffusion_coefficient[i][j][k][1] * dx[i][j][k] * dy[i][j][k] / Dr[i][j][k][1] * (1 - sh_adjacent_voxels[i][j][k][1])
             a_n = Diffusion_coefficient[i][j][k][2] * dx[i][j][k] * dz[i][j][k] / Dr[i][j][k][2] * (1 - sh_adjacent_voxels[i][j][k][2])
@@ -580,7 +580,7 @@ def boundary_condition_implicit_z_sweep_de_periodic(dt, gas_mass, Diffusion_coef
 
 
 @njit
-def set_matrices_rhs_y_sweep_de_periodic(n_y, i, k, rhs, gas_mass, z_sweep_gas_mass, surface, dx, dy, dz, Dr, Diffusion_coefficient, dt, S_c, sh_adjacent_voxels, n_x, temperature):
+def set_matrices_rhs_y_sweep_de_periodic(n_y, i, k, rhs, gas_mass, z_sweep_gas_mass, surface, dx, dy, dz, Dr, Diffusion_coefficient, dt, S_c, sh_adjacent_voxels, n_x, temperature, simulate_region):
     x_neg_periodic, x_pos_periodic = 0, 0
     if k == 1:
         x_neg_periodic = 1
@@ -592,7 +592,7 @@ def set_matrices_rhs_y_sweep_de_periodic(n_y, i, k, rhs, gas_mass, z_sweep_gas_m
             y_neg_periodic = 1
         if j == n_y - 2:
             y_pos_periodic = 1
-        if temperature[i][j][k] > 0 and np.sum(surface[i][j][k]) == 0 and rhs[j] == 0:
+        if (temperature[i][j][k] > 0 or simulate_region[i][j][k] == 1) and np.sum(surface[i][j][k]) == 0 and rhs[j] == 0:
             a_t = Diffusion_coefficient[i][j][k][0] * dx[i][j][k] * dy[i][j][k] / Dr[i][j][k][0] * (1 - sh_adjacent_voxels[i][j][k][0])
             a_b = Diffusion_coefficient[i][j][k][1] * dx[i][j][k] * dy[i][j][k] / Dr[i][j][k][1] * (1 - sh_adjacent_voxels[i][j][k][1])
             a_n = 1 / 2 * Diffusion_coefficient[i][j][k][2] * dx[i][j][k] * dz[i][j][k] / Dr[i][j][k][2] * (1 - sh_adjacent_voxels[i][j][k][2])
@@ -637,7 +637,7 @@ def boundary_condition_implicit_y_sweep_de_periodic(dt, gas_mass, z_sweep_gas_ma
 
 
 @njit
-def set_matrices_rhs_x_sweep_de_periodic(n_x, i, j, rhs, gas_mass, z_sweep_gas_mass, y_sweep_gas_mass, surface, dx, dy, dz, Dr, Diffusion_coefficient, dt, S_c, sh_adjacent_voxels, n_y, temperature):
+def set_matrices_rhs_x_sweep_de_periodic(n_x, i, j, rhs, gas_mass, z_sweep_gas_mass, y_sweep_gas_mass, surface, dx, dy, dz, Dr, Diffusion_coefficient, dt, S_c, sh_adjacent_voxels, n_y, temperature, simulate_region):
     y_neg_periodic, y_pos_periodic = 0, 0
     if j == 1:
         y_neg_periodic = 1
@@ -649,7 +649,7 @@ def set_matrices_rhs_x_sweep_de_periodic(n_x, i, j, rhs, gas_mass, z_sweep_gas_m
             x_neg_periodic = 1
         if k == n_x - 2:
             x_pos_periodic = 1
-        if temperature[i][j][k] > 0 and np.sum(surface[i][j][k]) == 0 and rhs[k] == 0:
+        if (temperature[i][j][k] > 0 or simulate_region[i][j][k] == 1) and np.sum(surface[i][j][k]) == 0 and rhs[k] == 0:
             a_t = Diffusion_coefficient[i][j][k][0] * dx[i][j][k] * dy[i][j][k] / Dr[i][j][k][0] * (1 - sh_adjacent_voxels[i][j][k][0])
             a_b = Diffusion_coefficient[i][j][k][1] * dx[i][j][k] * dy[i][j][k] / Dr[i][j][k][1] * (1 - sh_adjacent_voxels[i][j][k][1])
             a_n = Diffusion_coefficient[i][j][k][2] * dy[i][j][k] * dz[i][j][k] / Dr[i][j][k][2] * (1 - sh_adjacent_voxels[i][j][k][2])
@@ -694,7 +694,7 @@ def boundary_condition_implicit_x_sweep_de_periodic(dt, gas_mass, z_sweep_gas_ma
 
 
 @njit
-def de_implicit_DGADI_periodic(n_x, n_y, n_z, surface_reduced, dt, gas_mass, Diffusion_coefficient, Dr, dx, dy, dz, surface, S_c, S_p, sh_adjacent_voxels, top_layer_zero, temperature):
+def de_implicit_DGADI_periodic(n_x, n_y, n_z, surface_reduced, dt, gas_mass, Diffusion_coefficient, Dr, dx, dy, dz, surface, S_c, S_p, sh_adjacent_voxels, top_layer_zero, temperature, simulate_region):
     next_step_gas_mass = np.zeros((n_z, n_y, n_x), dtype=np.float64)
     z_sweep_gas_mass = np.zeros((n_z, n_y, n_x), dtype=np.float64)
     y_sweep_gas_mass = np.zeros((n_z, n_y, n_x), dtype=np.float64)
@@ -711,16 +711,17 @@ def de_implicit_DGADI_periodic(n_x, n_y, n_z, surface_reduced, dt, gas_mass, Dif
                     surface_elements_in_line[counter] = np.array([each[0], each[1], each[2]], dtype=np.float64)
                     counter += 1
             sub_alpha, diag, sub_gamma, rhs = boundary_condition_implicit_z_sweep_de_periodic(dt, gas_mass,  Diffusion_coefficient, Dr, dx, dy, dz, surface, surface_elements_in_line[0:counter], sub_alpha, diag, sub_gamma, rhs, S_c, S_p, sh_adjacent_voxels, top_layer_zero, n_x, n_y)
-            sub_alpha, diag, sub_gamma = set_matrices_lhs_z_sweep_de_periodic(n_z, j, k, sub_alpha, diag, sub_gamma, Diffusion_coefficient, dx, dy, dz, Dr, dt, S_p, gas_mass, surface, sh_adjacent_voxels, temperature)
-            rhs = set_matrices_rhs_z_sweep_de_periodic(n_z, j, k, rhs, gas_mass, surface, dx, dy, dz, Dr, Diffusion_coefficient, dt, S_c, sh_adjacent_voxels, n_x, n_y, temperature)
+            sub_alpha, diag, sub_gamma = set_matrices_lhs_z_sweep_de_periodic(n_z, j, k, sub_alpha, diag, sub_gamma, Diffusion_coefficient, dx, dy, dz, Dr, dt, S_p, gas_mass, surface, sh_adjacent_voxels, temperature, simulate_region)
+            rhs = set_matrices_rhs_z_sweep_de_periodic(n_z, j, k, rhs, gas_mass, surface, dx, dy, dz, Dr, Diffusion_coefficient, dt, S_c, sh_adjacent_voxels, n_x, n_y, temperature, simulate_region)
             z_sweep_gas_mass[1:n_z-1, j, k] = tridiagonal_matrix_solver(n_z-2, diag[1:n_z-1], sub_gamma[1:n_z-1], sub_alpha[1:n_z-1], rhs[1:n_z-1])
-            '''if j == 25 and k == 25:
+            '''if j == 12 and k == 12:
                 print(sub_gamma)
                 print(diag)
                 print(sub_alpha)
                 print(rhs)
                 print('z:', z_sweep_gas_mass[1:n_z-1, j, k])'''
     #print('z:', np.sum(z_sweep_gas_mass * dx * dy * dz))
+    #print(z_sweep_gas_mass[1, 7:17, 7:17])
     for i in range(1, n_z-1):
         for k in range(1, n_x-1):
             sub_alpha = np.zeros(n_y, dtype=np.float64)
@@ -734,11 +735,12 @@ def de_implicit_DGADI_periodic(n_x, n_y, n_z, surface_reduced, dt, gas_mass, Dif
                     surface_elements_in_line[counter] = np.array([each[0], each[1], each[2]], dtype=np.float64)
                     counter += 1
             sub_alpha, diag, sub_gamma, rhs = boundary_condition_implicit_y_sweep_de_periodic(dt, gas_mass, z_sweep_gas_mass, Diffusion_coefficient, Dr, dx, dy, dz, surface, surface_elements_in_line[0:counter], sub_alpha, diag, sub_gamma, rhs, S_c, S_p, sh_adjacent_voxels, top_layer_zero, n_x, n_y)
-            sub_alpha, diag, sub_gamma = set_matrices_lhs_y_sweep_de(n_y, i, k, sub_alpha, diag, sub_gamma, Diffusion_coefficient, dx, dy, dz, Dr, dt, S_p, gas_mass, surface, sh_adjacent_voxels, temperature)
-            rhs = set_matrices_rhs_y_sweep_de_periodic(n_y, i, k, rhs, gas_mass, z_sweep_gas_mass, surface, dx, dy, dz, Dr, Diffusion_coefficient, dt, S_c, sh_adjacent_voxels, n_x, temperature)
+            sub_alpha, diag, sub_gamma = set_matrices_lhs_y_sweep_de(n_y, i, k, sub_alpha, diag, sub_gamma, Diffusion_coefficient, dx, dy, dz, Dr, dt, S_p, gas_mass, surface, sh_adjacent_voxels, temperature, simulate_region)
+            rhs = set_matrices_rhs_y_sweep_de_periodic(n_y, i, k, rhs, gas_mass, z_sweep_gas_mass, surface, dx, dy, dz, Dr, Diffusion_coefficient, dt, S_c, sh_adjacent_voxels, n_x, temperature, simulate_region)
             y_sweep_gas_mass[i, 1:n_y-1, k] = periodic_tridiagonal_matrix_solver(n_y-2, diag[1:n_y-1], sub_gamma[1:n_y-1], sub_alpha[1:n_y-1], rhs[1:n_y-1])
     #print('y:', y_sweep_gas_mass[1:n_z-1, 25, 25])
     #print('y:', np.sum(y_sweep_gas_mass * dx * dy * dz))
+    #print(y_sweep_gas_mass[1, 7:17, 7:17])
     for i in range(1, n_z-1):
         for j in range(1, n_y-1):
             sub_alpha = np.zeros(n_x, dtype=np.float64)
@@ -752,9 +754,10 @@ def de_implicit_DGADI_periodic(n_x, n_y, n_z, surface_reduced, dt, gas_mass, Dif
                     surface_elements_in_line[counter] = np.array([each[0], each[1], each[2]], dtype=np.float64)
                     counter += 1
             sub_alpha, diag, sub_gamma, rhs = boundary_condition_implicit_x_sweep_de_periodic(dt, gas_mass, z_sweep_gas_mass, y_sweep_gas_mass, Diffusion_coefficient, Dr, dx, dy, dz, surface, surface_elements_in_line[0:counter], sub_alpha, diag, sub_gamma, rhs, S_c, S_p, sh_adjacent_voxels, top_layer_zero, n_x, n_y)
-            sub_alpha, diag, sub_gamma = set_matrices_lhs_x_sweep_de(n_x, i, j, sub_alpha, diag, sub_gamma, Diffusion_coefficient, dx, dy, dz, Dr, dt, S_p, gas_mass, surface, sh_adjacent_voxels, temperature)
-            rhs = set_matrices_rhs_x_sweep_de_periodic(n_x, i, j, rhs, gas_mass, z_sweep_gas_mass, y_sweep_gas_mass, surface,dx, dy, dz, Dr, Diffusion_coefficient, dt, S_c, sh_adjacent_voxels, n_y, temperature)
+            sub_alpha, diag, sub_gamma = set_matrices_lhs_x_sweep_de(n_x, i, j, sub_alpha, diag, sub_gamma, Diffusion_coefficient, dx, dy, dz, Dr, dt, S_p, gas_mass, surface, sh_adjacent_voxels, temperature, simulate_region)
+            rhs = set_matrices_rhs_x_sweep_de_periodic(n_x, i, j, rhs, gas_mass, z_sweep_gas_mass, y_sweep_gas_mass, surface,dx, dy, dz, Dr, Diffusion_coefficient, dt, S_c, sh_adjacent_voxels, n_y, temperature, simulate_region)
             next_step_gas_mass[i, j, 1:n_x-1] = periodic_tridiagonal_matrix_solver(n_x-2, diag[1:n_x-1], sub_gamma[1:n_x-1], sub_alpha[1:n_x-1], rhs[1:n_x-1])
     #print('nxt:', np.sum(next_step_gas_mass * dx * dy * dz))
     #print('nxt:', next_step_gas_mass[1:n_z - 1, 25, 25])
+    #print(next_step_gas_mass[1, 7:17, 7:17])
     return next_step_gas_mass

testing.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import constants as const
 import matplotlib.pyplot as plt
+import matplotlib.lines as line
 from matplotlib import rcParams
 import matplotlib.animation as animation
 from IPython.display import Video
@@ -354,7 +355,7 @@ with open('lamp_layer_absorption_factors_periodic300.json', 'w') as outfile:
     json.dump(data_dict, outfile)
 '''
 
-chi = [0.326, 0.1, 0.05, 0.01, 0.005, 0.001]
+'''chi = [0.326, 0.1, 0.05, 0.01, 0.005, 0.001]
 T_10 = [85.1647, 91.3946, 96.8681, 116.779, 128.6403, 158.08926]
 T_60 = [97.6746, 114.0831, 128.6047, 165.8284, 170.3352, 174.6266]
 
@@ -368,4 +369,71 @@ ax.set_ylabel('Temperature (K)')
 ax.set_title(r'Highest temperature with changing $\chi$')
 ax.set_xscale('log')
 plt.legend()
-plt.savefig('C:/Users/Christian Schuckart/OneDrive/Uni/Master/3 - Masterarbeit/Ice/chi_dependency.png', dpi=600)
+plt.savefig('C:/Users/Christian Schuckart/OneDrive/Uni/Master/3 - Masterarbeit/Ice/chi_dependency.png', dpi=600)'''
+
+'''with open('D:/TPM_Data/Noah/Outgassing_tests/middle_injection_dt_5e-8.json') as json_file:
+    jdata1 = json.load(json_file)
+with open('D:/TPM_Data/Noah/Outgassing_tests/middle_injection_dt_1e-7.json') as json_file:
+    jdata2 = json.load(json_file)
+with open('D:/TPM_Data/Noah/Outgassing_tests/middle_injection_dt_5e-7.json') as json_file:
+    jdata3 = json.load(json_file)
+with open('D:/TPM_Data/Noah/Outgassing_tests/middle_injection_dt_1e-6.json') as json_file:
+    jdata4 = json.load(json_file)
+
+time_5e8 = [i * 5E-8 for i in range(0, 2000)]
+time_1e7 = [i * 1E-7 for i in range(0, 1000)]
+time_5e7 = [i * 5E-7 for i in range(0, 200)]
+time_1e6 = [i * 1E-6 for i in range(0, 100)]
+fig, ax = plt.subplots(1, 1)
+plt.tick_params(axis='x', which='both', direction='in', top=True, labeltop=False)
+plt.tick_params(axis='y', which='both', direction='in', right=True, labelright=False)
+#ax.fill_between(time_5e8, jdata1['min'], jdata1['max'], label='dt = 5*10^-8', alpha=0.4)
+#ax.fill_between(time_1e7, jdata2['min'], jdata2['max'], ls='--', label='dt = 1*10^-7', alpha=0.4)
+#ax.fill_between(time_5e7, jdata3['min'], jdata3['max'], ls=':', label='dt = 5*10^-7', alpha=0.4)
+#ax.fill_between(time_1e6, jdata4['min'], jdata4['max'], ls='-.', label='dt = 1*10^-6', alpha=0.4)
+ax.plot(time_5e8, jdata1['max'], label='dt = 5*10^-8')
+ax.plot(time_1e7, jdata2['max'], ls='--', label='dt = 1*10^-7')
+ax.plot(time_5e7, jdata3['max'], ls=':', label='dt = 5*10^-7')
+ax.plot(time_1e6, jdata4['max'],  ls='-.', label='dt = 1*10^-6')
+#ax.plot()
+ax.set_xlabel('Time (s)')
+ax.set_ylabel('Gas density (kg/m^3)')
+ax.set_title('Max gas densities')
+ax.set_ylim(1E-21, 1E-14)
+ax.set_yscale('log')
+plt.legend()
+plt.savefig('D:/TPM_Data/Noah/Outgassing_tests/Max_gas_densities_zoom.png', dpi=600)
+plt.show()'''
+
+with open('D:/TPM_Data/Noah/Outgassing_tests/dynamic_production.json') as json_file:
+    data = json.load(json_file)
+
+fig, ax = plt.subplots(1, 1)
+time = [i for i in range(0, 300)]
+def update(t):
+    ax.clear()
+    #fig.clear()
+    a = ax.imshow(np.array(data['field'][t], dtype=np.float64)[0:const.n_z, 1:const.n_y-1])
+    l1 = line.Line2D([-0.5, 24], [1.5, 1.5], color='black', lw=4)
+    l2 = line.Line2D([-0.5, 24], [22.5, 22.5], color='black', lw=4)
+    l3 = line.Line2D([1.5, 1.5], [1.5, 22.5], color='black', lw=4)
+    l4 = line.Line2D([20.5, 20.5], [1.5, 22.5], color='black', lw=4)
+    l5 = line.Line2D([15.5, 15.5], [1.5, 22.5], color='black', lw=4)
+    '''static_lines = [l1, l2, l3, l4]
+    for each in static_lines:
+        ax.add_artist(each)
+    if t < 20:
+        ax.add_artist(l5)'''
+
+    #plt.colorbar(a)
+
+
+anim = animation.FuncAnimation(fig, update, frames=time, interval=200)
+
+#Writer = animation.writers['ffmpeg']
+Writer = animation.FFMpegWriter(fps=10, codec='mpeg4', bitrate=6000)
+#writer = Writer(fps=5, bitrate=1800)
+writer = Writer
+
+anim.save('D:/TPM_Data/Noah/Outgassing_tests/Vdynamic_production.mp4', writer=writer, dpi=600)
+Video('D:/TPM_Data/Noah/Outgassing_tests/Vdynamic_production.mp4')