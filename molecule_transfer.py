Updated calculation of pressure to use the correct particle areas (which now scale with volume/number)

molecule_transfer.py
@@ -817,7 +817,8 @@ def sinter_neck_calculation_time_dependent_diffusion(r_n, r_p, dt, temperature, 
                         areas[i][j][k] = (water_particle_number[i][j][k] * np.exp(r_c / r_p[i][j][k]) * 4 * np.pi * r_p[i][j][k] ** 2 + 3 * np.exp(-r_c / (r_n[i][j][k] * k_factor)) * neck_area)
                         sublimated_mass[i][j][k] = Z * (water_particle_number[i][j][k] * np.exp(r_c / r_p[i][j][k]) * 4 * np.pi * r_p[i][j][k] ** 2 + 3 * np.exp(-r_c / (r_n[i][j][k] * k_factor)) * neck_area) * dt
                         v = np.sqrt(8 * k_B * temperature[i][j][k] / (np.pi * m_H2O))
-                        pressure_impulse = 1/6 * 1 / (dx[i][j][k] * dy[i][j][k]) * sublimated_mass[i][j][k] * v**2 / (2 * r_p[i][j][k])    # 2 * r_mono ~~ mean free path
+                        pressure_impulse = 1 / areas[i][j][k] * sublimated_mass[i][j][k] * v ** 2 / (2 * r_p[i][j][k])  # 2 * r_mono ~~ mean free path
+                        #pressure_impulse = 1/6 * 1 / (dx[i][j][k] * dy[i][j][k]) * sublimated_mass[i][j][k] * v**2 / (2 * r_p[i][j][k])    # 2 * r_mono ~~ mean free path
                         if pressure_impulse > p_sub[i][j][k]:
                             sublimated_mass[i][j][k] = p_sub[i][j][k] * 6 * dx[i][j][k] * dy[i][j][k] * 2 * r_p[i][j][k] / (v**2) - (gas_density[i][j][k] * dx[i][j][k] * dy[i][j][k] * dz[i][j][k])
                         #areas[i][j][k] = (water_particle_number[i][j][k] * 4 * np.pi * r_p[i][j][k] ** 2)
@@ -875,7 +876,8 @@ def pressure_calculation_impulse(n_x, n_y, n_z, temperature, gas_density, k_bolt
             for c in range(0, n_x):
                 if temperature[a][b][c] > 0 and sample_holder[a][b][c] != 1 and areas[a][b][c] > 0:
                     v = np.sqrt(8 * k_boltzmann * temperature[a][b][c] / (np.pi * m_H2O))
-                    pressure[a][b][c] = 1/6 * 1 / (dx[a][b][c] * dy[a][b][c]) * (2 * r_mono[a][b][c]/dz[a][b][c]) * (gas_density[a][b][c] * dx[a][b][c] * dy[a][b][c] * dz[a][b][c]) * v**2 / (2 * r_mono[a][b][c])    # 2 * r_mono ~~ mean free path
+                    pressure[a][b][c] = 1/areas[a][b][c] * (2 * r_mono[a][b][c]/dz[a][b][c]) * (gas_density[a][b][c] * dx[a][b][c] * dy[a][b][c] * dz[a][b][c]) * v**2 / (2 * r_mono[a][b][c])    # 2 * r_mono ~~ mean free path
+                    #pressure[a][b][c] = 1/6 * 1 / (dx[a][b][c] * dy[a][b][c]) * (2 * r_mono[a][b][c]/dz[a][b][c]) * (gas_density[a][b][c] * dx[a][b][c] * dy[a][b][c] * dz[a][b][c]) * v**2 / (2 * r_mono[a][b][c])    # 2 * r_mono ~~ mean free path
     return pressure
 
 @njit

testing.py
@@ -541,13 +541,14 @@ lh_d_1 = np.array([-0.00320981,0.0194151,0.0529476])
 
 
 def p_sub(T, target):
+    print(10 ** (lh_a_1[0] + lh_b_1[0] / T + lh_c_1[0] * np.log10(T) + lh_d_1[0] * T))
     return 10 ** (lh_a_1[0] + lh_b_1[0] / T + lh_c_1[0] * np.log10(T) + lh_d_1[0] * T) - target
 
 target = 3
-val = brentq(p_sub, 150, 220, args=(target))[0]
+val = brentq(p_sub, 200, 250, args=(target))
 print(val)
 
-T = np.linspace(150, 220, 100)
+T = np.linspace(200, 250, 100)
 P = p_sub(T, 0)
 
 fig, ax = plt.subplots(1, 1)